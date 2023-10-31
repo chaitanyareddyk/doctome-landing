7946d6fc25d9a9d77927e358c8f15087757d7555
chaitanyareddyk <chaitanyareddy.kothapeta@gmail.com>
2023-10-31 23:53:26 +0530

    pixel code change

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -35,7 +35,7 @@ export default function Document() {
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '211456815259725');
+              fbq('init', '268934189465965');
               fbq('track', 'PageView');
           `,
             }}
@@ -46,7 +46,7 @@ export default function Document() {
               height="1"
               width="1"
               style={{ display: 'none' }}
-              src="https://www.facebook.com/tr?id=211456815259725&ev=PageView&noscript=1"
+              src="https://www.facebook.com/tr?id=268934189465965&ev=PageView&noscript=1"
             />
           </noscript>
         </>
